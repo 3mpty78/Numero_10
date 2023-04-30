65b82fe2e5962fc8aa5aa4963a42ec4efb588721
3mpty <aurelien.corre@outlook.fr>
2023-04-30 19:36:25 +0200

    2nd section style done

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-// Hamburger script
+document.addEventListener("DOMContentLoaded", function () {
+  var parent = document.querySelector(".splitview"),
+    topPanel = parent.querySelector(".top"),
+    handle = parent.querySelector(".handle"),
+    skewHack = 0,
+    delta = 0;
 
-const btn = document.querySelector(".nav-toggler");
-const header = document.querySelector(".header");
+  // If the parent has .skewed class, set the skewHack var.
+  if (parent.className.indexOf("skewed") != -1) {
+    skewHack = 1000;
+  }
 
-document.addEventListener("click", (e) => {
-  if (e.target.closest(".nav-toggler")) {
-    btn.classList.toggle("toggle");
-    header.classList.toggle("active");
-  } else if (!e.target.closest(".nav-toggler")) {
-    btn.classList.remove("toggle");
-    header.classList.remove("active");
-  }
+  parent.addEventListener("mousemove", function (event) {
+    // Get the delta between the mouse position and center point.
+    delta = (event.clientX - window.innerWidth / 2) * 0.5;
+
+    // Move the handle.
+    handle.style.left = event.clientX + delta + "px";
+
+    // Adjust the top panel width.
+    topPanel.style.width = event.clientX + skewHack + delta + "px";
+  });
 });
-document.addEventListener("scroll", (f) => {
-  if (window.scrollY > 50) {
-    btn.classList.remove("toggle");
-    header.classList.remove("active");
-  }
-});
+
+// Slider
